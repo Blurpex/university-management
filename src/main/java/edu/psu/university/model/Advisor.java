11eb6966ae88d32923c50de3f060b99779da9574
Haris Ahmad <harizahmad100@gmail.com>
2023-05-08 22:48:46 -0400

    advisor model

---
+++ src/main/java/edu/psu/university/model/Advisor.java
@@ -0,0 +1,28 @@
+package edu.psu.university.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="university")
+public class Advisor {
+
+    @Id
+    @Column(name="student_id")
+    private int studentId;
+
+    @Id
+    @Column(name="instructor_id")
+    private int instructorId;
+
+    public Advisor() {}
+
+    public Advisor(int studentId, int instructorId) {
+        this.studentId = studentId;
+        this.instructorId = instructorId;
+    }
+}
